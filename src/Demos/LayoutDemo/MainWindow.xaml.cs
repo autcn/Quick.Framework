9d69096b2362e26efb38bd9f5ae27960397c5c83
autcn <jackson316@sina.com>
2021-12-29 11:36:34 +0800

    添加布局工程

---
+++ src/Demos/LayoutDemo/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Quick;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayoutDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly IMessageBox _messageBox;
+        public MainWindow(IMessageBox messageBox)
+        {
+            _messageBox = messageBox;
+            InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            if (_messageBox.QuestionOKCancel("确定要退出吗？") == MessageBoxResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
